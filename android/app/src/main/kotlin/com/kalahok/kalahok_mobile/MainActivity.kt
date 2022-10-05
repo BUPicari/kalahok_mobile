5fc1c82c05fc54fcc85f74b4545539ea9ae0a28b
John Lemuel G. Hapa <jl.g.hapa@gmail.com>
2022-10-05 17:12:00 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/kalahok/kalahok_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kalahok.kalahok_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:kalahok_mobile/pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kalahok',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: AnimatedSplashScreen(
+        splash: Image.asset('assets/images/kalahok-logo.png'),
+        splashIconSize: double.infinity,
+        backgroundColor: Colors.indigo,
+        duration: 3000,
+        splashTransition: SplashTransition.fadeTransition,
+        nextScreen: const Home(title: "Kalahok"),
+      ),
+    );
+  }
+}
